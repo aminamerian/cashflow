165d4fe7c41c48adefcc3a02d936a109d0b60c66
Amin Amerian <aam.amin.am@gmail.com>
2023-07-30 22:46:16 +0330

    feat(accounts.urls): add basename to users url

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,7 +7,7 @@ from . import views
 app_name = 'accounts'
 
 router = routers.SimpleRouter()
-router.register('users', views.UserViewSet)
+router.register('users', views.UserViewSet, basename="users")
 
 urlpatterns = [
     path('', include(router.urls)),
